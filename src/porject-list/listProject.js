b5ee8007df8f856b1cebd551313e6eba51e13afb
Roman <115544124+Lukachyk@users.noreply.github.com>
2023-03-17 14:45:17 +0200

    fix link for projects

--- src/porject-list/listProject.js
+++ src/porject-list/listProject.js
@@ -1,22 +1,22 @@
 const listProjects = [
    {
       id: 1,
-      url: 'https://github.com/Lukachyk/Freelance-portfolio',
-      name: 'FR Portfolio',
+      url: 'https://lukachyk.github.io/parallax/',
+      name: 'Parallax ',
       datetime: 'Jan 18 2023',
       img: 'https://github.com/Lukachyk/about-me/blob/main/src/porject-list/imgProject/1-mini.jpeg?raw=true',
    },
    {
       id: 2,
-      url: 'https://github.com/Lukachyk/basket-react',
+      url: 'https://lukachyk.github.io/basket-react/',
       name: 'Basket React',
       datetime: 'Feb 4 2023',
       img: 'https://github.com/Lukachyk/about-me/blob/main/src/porject-list/imgProject/2-mini.jpeg?raw=true',
    },
    {
       id: 3,
-      url: 'https://github.com/Lukachyk/parallax',
-      name: 'Parallax',
+      url: 'https://lukachyk.github.io/Freelance-portfolio/',
+      name: 'FR Portfolio',
       datetime: 'Feb 16 2023',
       img: 'https://github.com/Lukachyk/about-me/blob/main/src/porject-list/imgProject/3-mini.jpeg?raw=true',
    },
